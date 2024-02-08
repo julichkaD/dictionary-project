6823520b114680128340d68aaeccf293547108f9
Julia Darwish <darwishjulia4@gmail.com>
2024-02-08 14:07:13 -0500

    Added header

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 import "./App.css";
 import Dictionary from "./Dictionary";
+import Icon from "./Icon.png";
 import Footer from "./Footer";
 
 function App() {
   return (
-    <div className="App">
-      <header>HELLO THERE!! </header>
+    <div>
+      <header>
+        <img src={Icon} alt="Dictionary" width={80} height={80} />
+        <span>DICTIONARY</span>
+      </header>
       <main>
         <Dictionary />
       </main>
-
       <Footer />
     </div>
   );
