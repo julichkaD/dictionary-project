b1384eb4c9dcaec3104e7a6f7a92624139964261
Julia Darwish <darwishjulia4@gmail.com>
2024-02-13 13:36:24 -0500

    Updated Meaning.js

--- src/Meaning.js
+++ src/Meaning.js
@@ -14,8 +14,8 @@ export default function Meaning(props) {
             <p>
               <strong>Definition:</strong> {definition.definition}
               <br />
-              <strong>Example:</strong>
-              <em> {definition.example}</em>
+              <em> 
+                {definition.example}</em>
               <br />
               <Synonyms synonyms={definition.synonyms} />
             </p>
