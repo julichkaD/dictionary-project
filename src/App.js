665244a876acd6dce60a1195b3037b36c83ed0d5
Julia Darwish <darwishjulia4@gmail.com>
2024-02-12 22:29:06 -0500

    Added MySwitch

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-import "./App.css";
 import Dictionary from "./Dictionary";
 import Icon from "./Icon.png";
 import Footer from "./Footer";
+import MySwitch from "./MySwitch";
 
 function App() {
   return (
     <div>
+      <MySwitch />
       <header>
         <img src={Icon} alt="Dictionary" width={80} height={80} />
         <span>DICTIONARY</span>
